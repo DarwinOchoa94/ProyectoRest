c2e5fa7ad4702c14b7b777ff20ec0b10c2d67204
Darwin Ochoa <darwin.ochoa94@gmail.com>
2020-08-24 17:50:44 -0500

    Cambios Varios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
 import lts_logo from './assets/images/lts_logo.png'
+import en_construccion from './assets/images/en_construccion_2.png'
 import './assets/css/App.css';
+import ReactPlayer from 'react-player'
 
 //Imports de Componentes
 import Componente01 from './componets/Componente01'
-
-
-//funcion creada para presentar los datos en html
-function Usuario(nombre, apellido, edad){
-  var Presentacion = <div>
-    <h2>Nombre: {nombre}</h2>
-    <h2>Apellido: {apellido}</h2>
-    <h2>Edad: {edad}</h2>
-  </div>
-  return Presentacion;
-}
+import ThemeSwitcher from './componets/ThemeSwitcher'
 
 function App() {
-  var nombre = "Darwin";
-  var apellido = "Ochoa"
-  var edad = "25";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={lts_logo} className="App-logo" alt="logo" />
-        <p>
-          {/*Edicion en tiempo real de aplicacion en React.*/}
-        <h1>
-          Hola Mundo!
-        </h1>
-          {Usuario (nombre, apellido, edad)}
-          <Componente01/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <body>
+          <div className="App-body">
+            <img src={lts_logo} className="App-logo" alt="logo" />
+            <ReactPlayer
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            />
+            <img src={en_construccion} className="App-logo" alt="En Contruccion"/>
+            </div>
+        </body>
     </div>
   );
 }
